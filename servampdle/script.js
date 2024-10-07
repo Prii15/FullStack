2cd656b43fd22504ef47c26d5cb1493c7fc1288e
Priscila Vazquez <priscilacv1402@gmail.com>
2024-10-07 20:14:21 -0300

    estilo servapdle

--- servampdle/script.js
+++ servampdle/script.js
@@ -1,64 +1,63 @@
 const personagens = [
     // Servamps
-    { nome: "Sleepy Ash", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Principal"},
-    { nome: "Old Child", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "Doubt-Doubt", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "The Mother", genero: "Feminino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "Lawless", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "World End", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "All of Love", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "Tsubaki", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário" },
+    { nome: "Sleepy Ash", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Principal", arcoNum: 1, altura: 1.77 },
+    { nome: "Old Child", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 0.80 },
+    { nome: "Doubt-Doubt", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 2.50 },
+    { nome: "The Mother", genero: "Feminino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 1.74 },
+    { nome: "Lawless", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 1.76 },
+    { nome: "World End", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 1.80 },
+    { nome: "All of Love", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 1.91 },
+    { nome: "Tsubaki", genero: "Masculino", tipo: "Vampiro", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 1.68 },
 
     // Eves
-    { nome: "Mahiru Shirota", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Principal" },
-    { nome: "Misono Alicein", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Principal" },
-    { nome: "Mikuni Alicein", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário" },
-    { nome: "Tetsu Sendagaya", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário" },
-    { nome: "Licht Jekylland Todoroki", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário" },
-    { nome: "Izuna Nobel", genero: "Feminino", tipo: "Humano", filiacao: "Eve", arco: "Secundário" },
-    { nome: "Niccolo Carpediem", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário" },
+    { nome: "Mahiru Shirota", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Principal", arcoNum: 1, altura: 1.65 },
+    { nome: "Misono Alicein", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Principal", arcoNum: 1, altura: 1.56 },
+    { nome: "Mikuni Alicein", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário", arcoNum: 2, altura: 1.74 },
+    { nome: "Tetsu Sendagaya", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário", arcoNum: 2, altura: 1.89 },
+    { nome: "Licht Jekylland Todoroki", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário", arcoNum: 2, altura: 1.75 },
+    { nome: "Izuna Nobel", genero: "Feminino", tipo: "Humano", filiacao: "Eve", arco: "Secundário", arcoNum: 2, altura: 1.64 },
+    { nome: "Niccolo Carpediem", genero: "Masculino", tipo: "Humano", filiacao: "Eve", arco: "Secundário", arcoNum: 2, altura: 1.78 },
 
     // Subclasses
-    { nome: "Belkia", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Gilberto Weasel", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Guildenstern", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Higan", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Lilac", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Marry", genero: "Feminino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Otogiri", genero: "Feminino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Rayscent Crazyrabbit", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Sagami", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Sakuya Watanuki", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Shamrock", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
-    { nome: "Yully", genero: "Feminino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário" },
+    { nome: "Belkia", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.84 },
+    { nome: "Gilberto Weasel", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.67 },
+    { nome: "Guildenstern", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.84 },
+    { nome: "Higan", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.95 },
+    { nome: "Lilac", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.60 },
+    { nome: "Marry", genero: "Feminino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.02 },
+    { nome: "Otogiri", genero: "Feminino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.58 },
+    { nome: "Rayscent Crazyrabbit", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.79 },
+    { nome: "Sakuya Watanuki", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.73 },
+    { nome: "Shamrock", genero: "Masculino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.89 },
+    { nome: "Yully", genero: "Feminino", tipo: "Vampiro", filiacao: "Subclass", arco: "Secundário", arcoNum: 2, altura: 1.02 },
 
     // C3-members
-    { nome: "Junichiro Kurumamori", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Shifumi Kurumamori", genero: "Feminino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Suuhei Tsuyuki", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Taishi Toma", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Tsurugi Kamiya", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Yoshimasa Tsuyuki", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Yumikage Tsukimitsu", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Iori Tsukimitsu", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
-    { nome: "Toru Shirota", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário" },
+    { nome: "Junichiro Kurumamori", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.85 },
+    { nome: "Suuhei Tsuyuki", genero: "Masculino", tipo: "Humano", filiacao: "C3r", arco: "Secundário", arcoNum: 2, altura: 1.78 },
+    { nome: "Taishi Toma", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.92 },
+    { nome: "Tsurugi Kamiya", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.70 },
+    { nome: "Yoshimasa Tsuyuki", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.78 },
+    { nome: "Yumikage Tsukimitsu", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.79 },
+    { nome: "Iori Tsukimitsu", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.81 },
+    { nome: "Toru Shirota", genero: "Masculino", tipo: "Humano", filiacao: "C3", arco: "Secundário", arcoNum: 2, altura: 1.80 },
 
     // Other Characters
-    { nome: "Gear", genero: "Masculino", tipo: "Lobisomem", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Inner Kuro", genero: "Masculino", tipo: "Other", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "Cappuccino", genero: "Masculino", tipo: "Humano", filiacao: "Máfia", arco: "Secundário" },
-    { nome: "Crantz Rosen", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Eisuke Dodo", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Johannes Mimir Faustus", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Koyuki", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Mikado Alicein", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Miyako Tsukimitsu", genero: "Feminino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Ophelia", genero: "Feminino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Ryusei", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Sensei", genero: "Masculino", tipo: "Other", filiacao: "Servamp", arco: "Secundário" },
-    { nome: "Takuto Kurumamori", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário" },
-    { nome: "Tiramisu", genero: "Masculino", tipo: "Humano", filiacao: "Máfia", arco: "Secundário" },
+    { nome: "Gear", genero: "Masculino", tipo: "Lobisomem", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.61 },
+    { nome: "Inner Kuro", genero: "Masculino", tipo: "Other", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: 1.77 },
+    { nome: "Cappuccino", genero: "Masculino", tipo: "Humano", filiacao: "Máfia", arco: "Secundário", arcoNum: 2, altura: 1.73 },
+    { nome: "Crantz Rosen", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.83 },
+    { nome: "Eisuke Dodo", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.75},
+    { nome: "Johannes Mimir Faustus", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 2.11},
+    { nome: "Koyuki", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.78},
+    { nome: "Mikado Alicein", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.75},
+    { nome: "Miyako Tsukimitsu", genero: "Feminino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.83},
+    { nome: "Ophelia", genero: "Feminino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.65},
+    { nome: "Ryusei", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.61},
+    { nome: "Sensei", genero: "Masculino", tipo: "Other", filiacao: "Servamp", arco: "Secundário", arcoNum: 2, altura: "??"},
+    { nome: "Takuto Kurumamori", genero: "Masculino", tipo: "Humano", filiacao: "Nenhuma", arco: "Secundário", arcoNum: 2, altura: 1.0},
+    { nome: "Tiramisu", genero: "Masculino", tipo: "Humano", filiacao: "Máfia", arco: "Secundário", arcoNum: 2, altura: 1.70},
 ];
+
 
 let sorteado;
 let personagemSorteado;
@@ -150,10 +149,27 @@ function mostraTentativas(personagem){
         filiacaoPerosnagem.style.backgroundColor = "lightcoral"; // Diferente
     }
     
+    const alturaPersonagem = document.createElement("div");
+    alturaPersonagem.className = "altura"; // Classe para estilizar cada sugestão
+    alturaPersonagem.textContent = personagem.altura; // Mostra o nome do personagem
+
+    // Muda a cor de fundo se a filiação for igual ao do personagem sorteado
+    if (personagem.altura == sorteado.altura) {
+        alturaPersonagem.style.backgroundColor = "lightgreen"; // Igual
+    } 
+    else if (personagem.altura > sorteado.altura) {
+        alturaPersonagem.style.backgroundColor = "lightaqua"; // Diferente
+    }
+
+    else {
+        alturaPersonagem.style.backgroundColor = "lightblue"; // Diferente
+    }
+
     tentativa.appendChild(nomePerosnagem);
     tentativa.appendChild(generoPerosnagem);
     tentativa.appendChild(tipoPerosnagem);
     tentativa.appendChild(filiacaoPerosnagem);
+    tentativa.appendChild(alturaPersonagem);
 
     // Adiciona a sugestão ao container
     tentativaContainer.appendChild(tentativa);
